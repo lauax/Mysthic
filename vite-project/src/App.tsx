a69e6abe47599e7e9f49ecd42ed7498769f6bdbd
lauax <lucas12alfredsson@gmail.com>
2023-08-09 14:52:08 +0200

    package

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import logo from '../images/MysticFarmandDistilleryLogo.png'
+import './App.css'
+function App() {
+  
+
+  return (
+   <>
+   <img src={logo} alt="Logo" style={{width:'125px', height:'100px',padding:'0'}}></img>
+   </>
+      
+  )
+}
+export default App
